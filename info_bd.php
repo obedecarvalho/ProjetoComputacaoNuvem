5a16a3f98543bbf78ce42e365f3a545ae3afeb2e
Obede Carvalho <obedejessecarvalho@gmail.com>
2017-07-28 14:05:57 -0300

    Atualizando .gitignore

--- info_bd.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $host = "127.0.0.1";
-    $login = "";
-    $senha = "";
-    $bd = "fastfoot";
-?>
--- session.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-    $logado = 0;
-    if(isset($_SESSION["id_usr"])){
-    	$logado = 1;
-	}
-    echo $logado;
-?>
