fb59446d8f3ba25290bb588bb81ba7156bbde9b7
Obede Carvalho <obedejessecarvalho@gmail.com>
2017-08-04 20:35:07 -0300

    Info db

--- site/info_bd.php
+++ site/info_bd.php
@@ -1,6 +1,7 @@
 <?php
     $host = "127.0.0.1";
-    $login = "root";
-    $senha = "admin";
-    $bd = "fastfoot";
+    $login = "";
+    $senha = "";
+    $bd = "";
+    $dbopts = "dbname=d91nlnphn6r8vv host=ec2-107-22-173-160.compute-1.amazonaws.com port=5432 user=dztrdyyjiskjaz password=eb4acea67715f142d90c544d580e096bc93c414f08125d8025adf236254fd84f sslmode=require";
 ?>
